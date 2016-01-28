a01eceda125344ea396d078ff88f45b82f341fe8
Cara Clarke <cara.clarke8@gmail.com>
2016-01-28 09:34:13 -0500

    More comments added

--- src/components/ListManager.jsx
+++ src/components/ListManager.jsx
@@ -4,6 +4,8 @@ var List = require('./List.jsx');
 var ListManager = React.createClass({
   // takes user input
 
+  // property of an object called getinitialstate, a function that is a property
+  // every component will call getinitialstate when the component first loads, only ever called once
   getInitialState: function() {
     return { items: [], newItemText: '' };
   },
@@ -13,10 +15,13 @@ var ListManager = React.createClass({
     // whenever user does keystroke, passes in element and changes state to be e.target.value
     // updates value box, tied to data source
   },
+  // function we created that is called when button is pressed to submit new data
   handleSubmit: function(e) {
     e.preventDefault();
+    // not using click function of button itself, don't want click to be accidently called thats why preventDefault
 
     var currentItems = this.state.items;
+    // components have properties and states
     // this.props is only ever read only
     // this.state is when you have data that can change (immutable data)
     // never mutate properties
@@ -48,7 +53,7 @@ var ListManager = React.createClass({
 
 module.exports = ListManager;
 
-// dynamic, reusable because props.title not 'Christmas To Do'
+// dynamic, reusable because props.title not ex:'Christmas To Do'
 // form knows this means to grab function not render function
 // input onChange called everytime keystroke put in input box
 // when user types we want it to show up in input box
